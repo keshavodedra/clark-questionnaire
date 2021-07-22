519acb4cf4b824e85bb4e4d6976a43255666476d
keshavodedara <kodedara@bloovo.com>
2021-07-22 19:27:11 +0400

    module commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
 import { QuestionnaireListComponent } from './questionnaire-list/questionnaire-list.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +17,9 @@ import { QuestionnaireListComponent } from './questionnaire-list/questionnaire-l
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
